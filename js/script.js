d14ce8635f6c3a94a2c329343c3ec43d3c014022
swatisingh88 <104416936+swatisingh88@users.noreply.github.com>
2022-04-26 12:30:07 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById("side-nav").classList.add('sidenav_open');
+  }
+  function closeNav() {
+    document.getElementById("side-nav").classList.remove('sidenav_open');
+  }
